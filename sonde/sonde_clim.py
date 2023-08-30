85974cdc1fe4d2bd2dbcd0e723526616783a67c2
AdamTheisen <atheisen@anl.gov>
2023-08-30 14:12:08 +0000

    NEW Data for SONDE/ACT Stats

--- sonde/sonde_clim.py
+++ sonde/sonde_clim.py
@@ -3,29 +3,54 @@ import act
 import numpy as np
 from scipy import stats
 import pandas as pd
+import dask
+
+
+def proc_data(f):
+    try:
+        ds = act.io.armfiles.read_netcdf(f, parallel=True)
+        ds = ds[['alt', 'wspd']] 
+        ds = ds.dropna(dim='time')
+        alt = ds['alt'].values
+        wspd = ds['wspd'].values
+    except:
+        print('Error with ' + f)
+        alt = [-9999.]
+        wspd = [-9999.]
+    return alt, wspd
 
 #files = glob.glob('/data/archive/sgp/sgpsondewnpnC1.b1/*20230801*')
 
 now = pd.Timestamp.now().to_period('m')
-dates = pd.period_range('2001-04-01', now-1, freq='M').strftime('%Y%m').tolist()
+dates = pd.period_range('2010-03-01', now-1, freq='M').strftime('%Y%m').tolist()
+
 
 bins = np.arange(0, 26000, 1000)
-df = pd.DataFrame(columns=bins[:-1])
 for d in dates:
+    df = pd.DataFrame(columns=bins[:-1])
     print(d)
     files = glob.glob('/data/archive/sgp/sgpsondewnpnC1.b1/*b1.' + d + '*')
     if len(files) <= 1:
         continue
     files.sort()
+    task = []
+    for f in files:
+        task.append(dask.delayed(proc_data)(f))
+    results = dask.compute(*task)
+    alt = []
+    wspd = []
+    for r1, r2 in results:
+        if len(r1) <= 1:
+            print('Results Error with' + f)
+            continue
+        alt = np.concatenate([alt, r1])
+        wspd = np.concatenate([wspd, r2])
+    bin_means, bin_edges, binnumber = stats.binned_statistic(alt, wspd, bins=bins)
     try:
-        ds = act.io.armfiles.read_netcdf(files, concat_dim='time', parallel=True)
-        ds = ds.dropna(dim='time')
-        bin_means, bin_edges, binnumber = stats.binned_statistic(ds['alt'].values, ds['wspd'].values, bins=bins)
-
-        ts = pd.to_datetime(d)
+        bin_means, bin_edges, binnumber = stats.binned_statistic(alt, wspd, bins=bins)
+        ts = pd.to_datetime(d, format='%Y%m')
         df.loc[ts] = bin_means
     except:
-        print('Failing')
+        print('Stats Error with ' + f)
         continue
-
-df.to_csv('data.csv')
+    df.to_csv('data.csv', mode='a')
