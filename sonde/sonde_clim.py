cf5c68df8115f134125befdd6d750c00fdc6bcd6
AdamTheisen <atheisen@anl.gov>
2023-08-30 10:25:08 -0500

    ENH Updating

--- sonde/sonde_clim.py
+++ sonde/sonde_clim.py
@@ -6,28 +6,29 @@ import pandas as pd
 import dask
 
 
-def proc_data(f):
+def proc_data(f, variables):
     try:
         ds = act.io.armfiles.read_netcdf(f, parallel=True)
-        ds = ds[['alt', 'wspd']] 
+        ds = ds[variables]
         ds = ds.dropna(dim='time')
-        alt = ds['alt'].values
-        wspd = ds['wspd'].values
+        data = {}
+        for v in variables:
+            data[v] = ds[v].values
     except:
         print('Error with ' + f)
-        alt = [-9999.]
-        wspd = [-9999.]
-    return alt, wspd
+        for v in variables:
+            data[v] = -9999
+    return data
 
 #files = glob.glob('/data/archive/sgp/sgpsondewnpnC1.b1/*20230801*')
 
 now = pd.Timestamp.now().to_period('m')
-dates = pd.period_range('2010-03-01', now-1, freq='M').strftime('%Y%m').tolist()
+dates = pd.period_range('2001-04-01', now-1, freq='M').strftime('%Y%m').tolist()
 
 
-bins = np.arange(0, 26000, 1000)
+bins = np.arange(0, 26000, 500)
+variables = ['alt', 'wspd', 'tdry', 'rh']
 for d in dates:
-    df = pd.DataFrame(columns=bins[:-1])
     print(d)
     files = glob.glob('/data/archive/sgp/sgpsondewnpnC1.b1/*b1.' + d + '*')
     if len(files) <= 1:
@@ -35,22 +36,33 @@ for d in dates:
     files.sort()
     task = []
     for f in files:
-        task.append(dask.delayed(proc_data)(f))
+        task.append(dask.delayed(proc_data)(f, variables))
     results = dask.compute(*task)
-    alt = []
-    wspd = []
-    for r1, r2 in results:
+
+    # Get data from dict
+    dat = {}
+    for v in variables:
+        dat[v] = []    
+        dat['time'] = []
+    for r1 in results:
         if len(r1) <= 1:
             print('Results Error with' + f)
             continue
-        alt = np.concatenate([alt, r1])
-        wspd = np.concatenate([wspd, r2])
-    bin_means, bin_edges, binnumber = stats.binned_statistic(alt, wspd, bins=bins)
+        for v in variables:
+            dat[v] = np.concatenate([data[v], r1[v]])
+    print(dat)
+    bin_data = {}
+    for v in variables + ['time', 'count']:
+        bin_data[v] = []
     try:
-        bin_means, bin_edges, binnumber = stats.binned_statistic(alt, wspd, bins=bins)
         ts = pd.to_datetime(d, format='%Y%m')
-        df.loc[ts] = bin_means
+        bin_data['time'] = np.concatenate([data['time'], ts])
+        bin_data['count'] = np.concatenate([data['count'], len(files)])
+        print(bin_data)
+        sys.exit()
+        for v in variables:
+            bin_means, bin_edges, bin_number = stats.binned_statistic(dat['alt'], dat[v], bins=bins)
+        
     except:
         print('Stats Error with ' + f)
         continue
-    df.to_csv('data.csv', mode='a')
