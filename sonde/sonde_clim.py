06751f71deb7e7bacdc91668d5fbadfcc4120a42
AdamTheisen <atheisen@anl.gov>
2023-08-31 18:46:45 +0000

    ENH: New data and updates

--- sonde/sonde_clim.py
+++ sonde/sonde_clim.py
@@ -4,20 +4,22 @@ import numpy as np
 from scipy import stats
 import pandas as pd
 import dask
+import xarray as xr
 
 
 def proc_data(f, variables):
+    data = {}
     try:
         ds = act.io.armfiles.read_netcdf(f, parallel=True)
         ds = ds[variables]
+        old_ds = ds
         ds = ds.dropna(dim='time')
-        data = {}
         for v in variables:
             data[v] = ds[v].values
     except:
         print('Error with ' + f)
         for v in variables:
-            data[v] = -9999
+            data[v] = [-9999]
     return data
 
 #files = glob.glob('/data/archive/sgp/sgpsondewnpnC1.b1/*20230801*')
@@ -26,8 +28,16 @@ now = pd.Timestamp.now().to_period('m')
 dates = pd.period_range('2001-04-01', now-1, freq='M').strftime('%Y%m').tolist()
 
 
-bins = np.arange(0, 26000, 500)
-variables = ['alt', 'wspd', 'tdry', 'rh']
+bins = np.arange(0, 26000., 500.)
+variables = ['alt', 'wspd', 'tdry', 'rh', 'dp', 'u_wind', 'v_wind', 'asc', 'pres']
+
+bin_data = {}
+for v in ['time', 'count']:
+    bin_data[v] = {'data': [], 'dims': ['time']}
+for v in variables:
+    bin_data[v] = {'data': [], 'dims': ['time', 'height']}
+bin_data['height'] = {'data': bins[0:-1], 'dims': ['height']}
+
 for d in dates:
     print(d)
     files = glob.glob('/data/archive/sgp/sgpsondewnpnC1.b1/*b1.' + d + '*')
@@ -44,25 +54,29 @@ for d in dates:
     for v in variables:
         dat[v] = []    
         dat['time'] = []
+    ct = 0
     for r1 in results:
-        if len(r1) <= 1:
-            print('Results Error with' + f)
+        if len(r1['alt']) <= 1:
+            print('Results Error')
             continue
+        ct += 1
         for v in variables:
-            dat[v] = np.concatenate([data[v], r1[v]])
-    print(dat)
-    bin_data = {}
-    for v in variables + ['time', 'count']:
-        bin_data[v] = []
+            dat[v] = np.concatenate([dat[v], r1[v]])
+    
     try:
         ts = pd.to_datetime(d, format='%Y%m')
-        bin_data['time'] = np.concatenate([data['time'], ts])
-        bin_data['count'] = np.concatenate([data['count'], len(files)])
-        print(bin_data)
-        sys.exit()
-        for v in variables:
+        bin_data['time']['data'] = np.concatenate([bin_data['time']['data'], [ts]])
+        bin_data['count']['data'] = np.concatenate([bin_data['count']['data'], [ct]])
+
+        for v in variables[0:]:
             bin_means, bin_edges, bin_number = stats.binned_statistic(dat['alt'], dat[v], bins=bins)
-        
+            if len(bin_data[v]['data']) == 0:
+                bin_data[v]['data'] = bin_means
+            else:
+                bin_data[v]['data'] = np.vstack([bin_data[v]['data'], bin_means])
     except:
         print('Stats Error with ' + f)
         continue
+
+ds = xr.Dataset.from_dict(bin_data)
+ds.to_netcdf('sgpsondeC1.b1.nc')
