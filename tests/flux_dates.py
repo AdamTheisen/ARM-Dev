accc02123b895204196d7585e64da2a8f3ba7b91
AdamTheisen <atheisen@anl.gov>
2024-10-28 12:00:09 +0000

    Cronjob commit

--- tests/flux_dates.py
+++ tests/flux_dates.py
@@ -1,11 +1,11 @@
 import glob
 import act
 
-dirs = glob.glob('/data/archive/sgp/sgp5ebbr*b1*')
+dirs = glob.glob('/data/archive/*/*ecor*b1*')
 dirs.sort()
 for d in dirs:
     files = glob.glob(d + '/*')
     files.sort()
     obj = act.utils.DatastreamParserARM(files[0])
     e_obj = act.utils.DatastreamParserARM(files[-1])
-    print(obj.site, obj.facility, obj.date, e_obj.date)
+    print(obj.site, obj.datastream_class, obj.facility, obj.date, e_obj.date)
