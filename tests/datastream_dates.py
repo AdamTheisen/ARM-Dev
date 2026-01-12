c249002f93d9681d3eb4ed65bea9c3306f37f2e4
AdamTheisen <atheisen@anl.gov>
2026-01-12 12:00:10 +0000

    Cronjob commit

--- tests/datastream_dates.py
+++ tests/datastream_dates.py
@@ -1,11 +1,14 @@
 import glob
 import act
 
-dirs = glob.glob('/data/archive/*/*ecor*b1*')
+dirs = glob.glob('/data/archive/*/*hsrl*1*')
 dirs.sort()
 for d in dirs:
     files = glob.glob(d + '/*')
     files.sort()
+    if len(files) == 0:
+        print(d)
+        continue
     obj = act.utils.DatastreamParserARM(files[0])
     e_obj = act.utils.DatastreamParserARM(files[-1])
     print(obj.site, obj.datastream_class, obj.facility, obj.date, e_obj.date)
--- tests/n_samples.py
+++ tests/n_samples.py
@@ -1,7 +1,14 @@
 import act
 import glob
+from scipy.stats import mode
 
-files = glob.glob('/data/archive/sgp/sgppgsC1.b1/*.2023*')
-ds = act.io.read_arm_netcdf(files)
+files = glob.glob('/data/archive/bnf/bnfhsrlM1.a1/*.202509*')
+files.sort()
+data = []
+for f in files:
+    ds = act.io.read_arm_netcdf(f)
 
-print(ds)
+    print(len(ds['time'].values))
+    data.append(len(ds['time'].values))
+
+print(mode(data))
