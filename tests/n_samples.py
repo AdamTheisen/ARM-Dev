e4d771840c136c74c602568f6c13de8338275126
AdamTheisen <atheisen@anl.gov>
2026-02-09 12:00:09 +0000

    Cronjob commit

--- tests/n_samples.py
+++ tests/n_samples.py
@@ -1,14 +1,25 @@
 import act
 import glob
 from scipy.stats import mode
+import matplotlib.pyplot as plt
 
-files = glob.glob('/data/archive/bnf/bnfhsrlM1.a1/*.202509*')
+#files = glob.glob('/data/archive/bnf/bnfaosccn2colaavgM1.b1/*.2025*')
+files = glob.glob('/data/archive/bnf/bnfaosccn200M1.a1/*.2025*')
 files.sort()
 data = []
+time = []
 for f in files:
     ds = act.io.read_arm_netcdf(f)
 
-    print(len(ds['time'].values))
     data.append(len(ds['time'].values))
+    time.append(ds['time'].values[0])
 
-print(mode(data))
+
+fig, ax = plt.subplots(figsize=(15,10))
+ax.plot(time, data)
+
+# Save figure
+plt.savefig('/data/www/userplots/theisen/n_samples.png')
+plt.clf()
+
+
